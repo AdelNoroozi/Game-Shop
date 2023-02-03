0e67fe8aee84cde9ca273802429ed166f981a82e
Adel <adel13no@gmail.com>
2023-02-03 15:20:42 +0330

    mobile views modified

--- game_shop_vue/src/App.vue
+++ game_shop_vue/src/App.vue
@@ -3,13 +3,14 @@
     <nav class="navbar is-dark">
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>San Andreas Games</strong></router-link>
-        <a class="navbar-burger" aria-label="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+           @click="showMobileMenu=!showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active':showMobileMenu}">
         <div class="navbar-end">
           <router-link to="/disk" class="navbar-item">Disk</router-link>
           <router-link to="/digital" class="navbar-item">Digital</router-link>
@@ -32,6 +33,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
