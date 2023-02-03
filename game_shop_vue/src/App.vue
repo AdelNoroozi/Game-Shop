5579e1e39d2de427c31e99ffd3e0fb18c06886d2
Adel <adel13no@gmail.com>
2023-02-03 15:09:42 +0330

    base template created with vue

--- game_shop_vue/src/App.vue
+++ game_shop_vue/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrappr">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>San Andreas Games</strong></router-link>
+        <a class="navbar-burger" aria-label="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/disk" class="navbar-item">Disk</router-link>
+          <router-link to="/digital" class="navbar-item">Digital</router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <router-view/>
+    </section>
+    <footer class="has-text-centered">
+      <p>Created by Adel</p>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "../node_modules/bulma";
 </style>
